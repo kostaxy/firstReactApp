b09379db315f2130b40c509fc6df011fe23db820
kostaxy <kshandrak@yandex.by>
2022-01-09 18:14:57 +0300

    initialization

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react'
+import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+import './styles/App.css';
+
+function App() {
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'js', body: 'Description' },
+        { id: 2, title: 'c', body: 'Description' },
+        { id: 3, title: 'java', body: 'Description' },
+        { id: 4, title: 'python', body: 'Description' }
+    ]);
+
+    const [post, setPost] = useState({body: '', title: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        setPosts([...posts, {...post, id: Date.now()}])
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    text='text'
+                    placeholder='Название поста'
+                />
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    text='text'
+                    placeholder='Текст поста'   
+                />
+                <MyButton onClick={addNewPost}>text</MyButton>
+            </form>
+            <PostList posts={posts} />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={() => setCounter(counter + 1)}>
+                Increment
+            </button>
+            <button onClick={() => setCounter(counter - 1)}>
+                Decrement
+            </button>
+        </div>
+    )
+}
+
+export default Counter
