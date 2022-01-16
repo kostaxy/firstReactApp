81f1ae092d9cfb0a724d836fce3b01ee29e941ee
kostaxy <kshandrak@yandex.by>
2022-01-16 18:54:52 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import PostService from './API/PostService';
+import { useFetch } from './components/hooks/useFetch';
 import { usePosts } from './components/hooks/usePosts';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
@@ -8,7 +9,9 @@ import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import Loader from './components/UI/Loader/Loader';
 import Modal from './components/UI/Modal/Modal';
+import Pagination from './components/UI/pagination/Pagination';
 import './styles/App.css';
+import { getPageCount} from './utils/pages';
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -16,11 +19,33 @@ function App() {
     const [modal, setModal] = useState(false)
     const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
-    const [isLoadingPosts, setIsLoadingPosts] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+
+    const [fetchPosts, isLoadingPosts, postError] = useFetch(
+        async () => {
+            const response = await PostService.getAll(limit, page);
+            setPosts(response.data)
+            const totalCount = response.headers['x-total-count']
+            setTotalPages(getPageCount(totalCount, limit))
+        }
+    )
+
+    const changePage = (page_num) =>{
+        setPage(page_num)
+    }
+
+    // useMemo(() => {
+    //     for (let i = 0; i < totalPages; i++) {
+    //         pagesArray.push(i + 1)
+    //     }
+    // }, [totalPages])
+
 
     useEffect(() => {
         fetchPosts();
-    }, [])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -29,16 +54,6 @@ function App() {
 
     const removePost = (removePost) => {
         setPosts(posts.filter(p => removePost.id !== p.id))
-    }
-
-    async function fetchPosts() {
-        setIsLoadingPosts(true)
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts)
-            setIsLoadingPosts(false)
-        }, 1000);
-
     }
 
     return (
@@ -56,10 +71,14 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            {postError &&
+                <h1>Произошла ошибка {postError}</h1>
+            }
             {isLoadingPosts
                 ? <Loader />
                 : <PostList posts={sortedAndSearchPosts} remove={removePost} />
             }
+            <Pagination page={page} changePage={changePage} totalPages={totalPages}></Pagination>
         </div>
     );
 }
